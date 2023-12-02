96a8b0f07aedeb2a6bb81367d721f095bc07b0a9
Mario M <107927311+19mario19@users.noreply.github.com>
2023-12-02 06:21:50 +0000

    added interface

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
+<script lang="ts">
+    interface Person {
+        name: string,
+        age: number
+    }
+
+    let person : Person 
+    person = {
+        name: "Alina",
+        age: 22,
+    }
+</script>
+
 <h1>Welcome to SvelteKit</h1>
+<div class="person">
+    <p>{person.name} - {person.age}</p>
+</div>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
